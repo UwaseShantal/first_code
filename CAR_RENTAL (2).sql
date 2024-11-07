6917ea5764f9b2dcd4ceb9724868859cf6aa68e3
UwaseShantal <uwaseshantal8@gmail.com>
2024-11-07 17:34:00 +0300

    Add files via upload

---
+++ CAR_RENTAL (2).sql
@@ -0,0 +1,7 @@
+-- Active: 1728667256342@@127.0.0.1@3306@car_rental
+SHOW DATABASES;
+USE CAR_RENTAL;
+SELECT * FROM affiliatedcompanies;
+SELECT * FROM customer;
+SELECT * FROM rentalorganization;
+SELECT * FROM vehicle;
