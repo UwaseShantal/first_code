2e6d623ac7252f9632e4e9c980253f8aefdbe1c6
UwaseShantal <uwaseshantal8@gmail.com>
2024-11-09 10:32:01 +0300

    Add files via upload

---
+++ commit6.js
@@ -0,0 +1,10 @@
+//a function that takes an array of numbers and returns the sum
+function sumArray(numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+console.log(sumArray([1,2,3,4,5,]));//15
+
+ 
+ 
+ 
+ 
